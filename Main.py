dca84e8ffe937207fa8a3607fdc3c0f4b7f754f5
SharPinG <spg1101@naver.com>
2022-07-18 07:30:55 +0900

    m

--- Main.py
+++ Main.py
@@ -1,17 +1,56 @@
-import sys;
-from PyQt5.QtWidgets import QApplication, QWidget;
+import sys
+import subprocess
 
-# pip install pyqt5
-# pip install PyQt5-tools
+try:
+    from PyQt5.uic import loadUi
+    from PyQt5 import uic, QtWidgets
+    from PyQt5.QtWidgets import QDialog, QApplication
 
-class Main(QWidget):
+except:
+    subprocess.check_call([sys.executable,'-m', 'pip', 'install', '--upgrade', 'pip']);
+
+    subprocess.check_call([sys.executable,'-m', 'pip', 'install', '--upgrade', 'pyqt5']);
+    subprocess.check_call([sys.executable,'-m', 'pip', 'install', '--upgrade', 'PyQt5-tools']);
+
+    from PyQt5.uic import loadUi
+    from PyQt5 import uic, QtWidgets
+    from PyQt5.QtWidgets import QDialog, QApplication
+
+
+class FirstController(QDialog):
     def __init__(self):
-        super().__init__();
-        self.initUI();
-    
-    def initUI(self):
-        self.show();
+        super(FirstController, self).__init__();
+        loadUi("View/firstView.ui", self);
+        self.btnGoToSecond.clicked.connect(self.btn_clicked);
+        
+    def btn_clicked(self):
+        secondView = SecondController();
+        widget.addWidget(secondView);
+        widget.setCurrentIndex(widget.currentIndex()+1);
 
-app = QApplication(sys.argv);
-w = Main();
-sys.exit(app.exec_());
+
+class SecondController(QDialog):
+    def __init__(self):
+        super(SecondController, self).__init__();
+        loadUi("View/secondView.ui", self);
+        self.btnGoToFirst.clicked.connect(self.btn_clicked);
+
+    def btn_clicked(self):
+        widget.removeWidget(self);
+        widget.setCurrentIndex(widget.currentIndex()-1);
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv);
+    widget = QtWidgets.QStackedWidget();
+
+    firstView = FirstController();
+    widget.addWidget(firstView);
+    widget.setFixedWidth(400);
+    widget.setFixedHeight(400);
+    widget.show();
+
+    try:
+        sys.exit(app.exec_());
+    except:
+        print("");
